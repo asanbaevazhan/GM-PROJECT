b5ad04063c432686fd82b7069fb2762b0eb0973e
vhhhhq <askarbekovanuraida@gmail.com>
2022-10-27 19:00:42 +0300

    scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header.jsx';
 import Home from './pages/Home/Home.jsx';
